39586892496d4b7e8f49be10e0cb5fd07161717e
Ali UYSUN <ali_uysun45@hotmail.com>
2025-05-08 17:19:21 +0300

    yedeklenmişhalinideCommitledim

---
+++ 24NisanVeritabanıBaglantıSonHali/StokTakip/StokTakip/Controllers/KategoriController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokTakip.Models.StokModel;
+
+namespace StokTakip.Controllers
+{
+    public class KategoriController : Controller
+    {
+        UrunStokBaglan bag = new UrunStokBaglan();
+
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var liste = bag.KATEGORI.ToList();
+            return View(liste);
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(KATEGORI p)
+        {
+
+            bag.KATEGORI.Add(p);
+            bag.SaveChanges();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+    }
+}
